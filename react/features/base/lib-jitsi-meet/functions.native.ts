0110d88360673587694464759fb8bf39fb741227
ahmad <a.khalili137190@gmail.com>
2024-12-23 10:02:52 +0330

    add axios and change logo

--- react/features/base/lib-jitsi-meet/functions.native.ts
+++ react/features/base/lib-jitsi-meet/functions.native.ts
@@ -5,6 +5,7 @@ import { NativeModules } from 'react-native';
 import { loadScript } from '../util/loadScript.native';
 
 import logger from './logger';
+import axios from "axios";
 
 export * from './functions.any';
 
@@ -18,12 +19,37 @@ const { JavaScriptSandbox } = NativeModules;
  */
 export async function loadConfig(url: string): Promise<Object> {
     try {
-
-        // const response = await fetch(url );
-        // // @ts-ignore
-        // throw new Error('Failed to load config from @@@',response.text.toString());
-
-
+        // axios(
+        //     {
+        //         // url: baseUrl + url + params,
+        //        // url:  url,
+        //         //url:  "https://vc2.satia.co/config.js",
+        //         url:  "https://app137.satia.co/",
+        //         //url:  "https://alpha.jitsi.net/config.js",
+        //         method: "get",
+        //         //params: params,
+        //         timeout: 60 * 1000,
+        //         headers: {
+        //             'Content-Type': 'application/x-www-form-urlencoded',
+        //             // "Authorization": token,
+        //         },
+        //         /*      headers: { "Authorization": params._session?params._session:params.token, 'X-Requested-With': 'XMLHttpRequest'},*/
+        //     },
+        // )
+        //     .then(async response => {
+        //
+        //         let data = response.data;
+        //         // @ts-ignore
+        //         throw new Error('Failed to load config from @@@',data.toString());
+        //
+        //         // @ts-ignore
+        //       //  throw new Error('Failed to load config from @@@',response.text.toString());
+        //
+        //     }).catch((e) => {
+        //     console.log(e);
+        //     logger.error(`Failed to load config from ??? ${url}`, e);
+        // });
+        // return ""
 
         const configTxt = await loadScript(url, 10 * 1000 /* Timeout in ms */, true /* skipeval */);
         const configJson = await JavaScriptSandbox.evaluate(`${configTxt}\nJSON.stringify(config);`);
